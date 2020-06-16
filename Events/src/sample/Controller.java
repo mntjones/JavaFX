c025b16a1ad4353668612099667eb431e95caca0
Monica Tamaru Jones <mntamaru@gmail.com>
2020-06-16 08:49:54 -0700

    buttons enabled to print out messages

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,14 +1,40 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button goodbyeButton;
 
     @FXML
-    public void onButtonClicked() {
-        System.out.println("Hello, " + nameField.getText());
+    public void initialize() {
+        helloButton.setDisable(true);
+        goodbyeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if(e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        }
+        else if (e.getSource().equals(goodbyeButton)) {
+            System.out.println("Goodbye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        goodbyeButton.setDisable(disableButtons);
     }
 }
