7af63fe62a7fab8fee8895ca16c5403eb49d265a
Monica Tamaru Jones <mntamaru@gmail.com>
2020-06-17 16:51:19 -0700

    add css style sheet to an application

---
+++  JavaFXApplication/src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 225));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+// Order or Precedence of CSS
+// 1. In-line CSS
+// 2. FXML style sheet
+// 3. Applications theme style sheet (Main)
