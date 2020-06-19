57e468b3fef5d964f744d269551089f3d5a152cb
Monica Tamaru Jones <mntamaru@gmail.com>
2020-06-18 18:39:15 -0700

    add fxml code

---
+++ JavaFXChallenge/src/module-info.java
@@ -0,0 +1,7 @@
+module JavaFXChallenge {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.xml;
+
+    opens sample;
+}
--- JavaFXChallenge/src/sample/Controller.java
+++ JavaFXChallenge/src/sample/Controller.java
@@ -1,23 +1,18 @@
 package sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.layout.GridPane;
-
-import javax.swing.text.TableView;
-import java.util.List;
 
 public class Controller {
 
-    private List<Contact> contacts;
-
-    @FXML
-    private GridPane gridPane;
-    @FXML
-    private TableView tableView;
-
-    public void initialize() {
-
-    }
+//    private List<Contact> contacts;
+//
+//    @FXML
+//    private BorderPane borderPane;
+//
+//
+//
+//    public void initialize() {
+//
+//    }
 
 
 }
